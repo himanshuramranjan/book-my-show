4875c578fedfea3f9c98bae34027731f9b42d342
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-07-11 23:52:42 +0530

    seat locking added for multi threading issue

--- src/model/Seat.java
+++ src/model/Seat.java
@@ -7,12 +7,14 @@ public class Seat {
     private final String seatLoc; // e.g 4A, 8C
     private final SeatCategory seatCategory;
     private boolean isBooked;
+    private boolean isLocked;
 
     public Seat(String seatId, String seatLoc, SeatCategory seatCategory) {
         this.seatId = seatId;
         this.seatLoc = seatLoc;
         this.seatCategory = seatCategory;
         this.isBooked = false;
+        this.isLocked = false;
     }
 
     public String getSeatLoc() {
@@ -27,11 +29,24 @@ public class Seat {
         return seatCategory;
     }
 
-    public boolean isBooked() {
+    public synchronized boolean isBooked() {
         return isBooked;
     }
 
-    public void bookSeat(boolean booked) {
-        isBooked = booked;
+    public synchronized boolean isLocked() { return isLocked; }
+
+    public synchronized boolean tryLockSeat() {
+        if(isLocked || isBooked) {
+            return false;
+        }
+        isLocked = true;
+        return  true;
+    }
+
+    public synchronized void unlockSeat() { isLocked = false; }
+
+    public synchronized void confirmBooking() {
+        isBooked = true;
+        isLocked = false;
     }
 }
